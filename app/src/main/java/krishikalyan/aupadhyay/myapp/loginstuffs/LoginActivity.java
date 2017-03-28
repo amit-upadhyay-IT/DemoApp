a3f39c8adea64113d946712f89e3e2dbfca7132b
amit-upadhyay-IT <youarefreak1@gmail.com>
2017-03-28 16:08:20 +0530

    login activity added

---
+++ app/src/main/java/krishikalyan/aupadhyay/myapp/loginstuffs/LoginActivity.java
@@ -0,0 +1,15 @@
+package krishikalyan.aupadhyay.myapp.loginstuffs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import krishikalyan.aupadhyay.myapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
