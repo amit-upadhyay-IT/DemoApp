7d2ab94adc864357023a3281fd29c9e73205db21
amit-upadhyay-IT <youarefreak1@gmail.com>
2017-03-28 14:08:27 +0530

    HomeActivity added

---
+++ app/src/main/java/krishikalyan/aupadhyay/myapp/activities/HomeActivity.java
@@ -0,0 +1,15 @@
+package krishikalyan.aupadhyay.myapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import krishikalyan.aupadhyay.myapp.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
